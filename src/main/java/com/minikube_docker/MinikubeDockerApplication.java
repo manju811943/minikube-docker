2be0e566ef767f307f82634ec7823924a732266e
manju811943 <manju811943@gmail.com>
2025-12-03 00:06:45 +0530

    issue with messag in controller class

--- src/main/java/com/minikube_docker/MinikubeDockerApplication.java
+++ src/main/java/com/minikube_docker/MinikubeDockerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MinikubeDockerApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(MinikubeDockerApplication.class, args);
 	}
